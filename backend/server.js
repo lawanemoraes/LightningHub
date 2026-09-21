9c9b63c8946f92f3fb2e1677444c8bd8ff77840d
Lawane Moraes <lawane.moraes@fatecitapetininga.edu.br>
2026-09-21 14:28:09 -0300

    Inicializar backend com Node e Express

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("LightningHub API funcionando!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
